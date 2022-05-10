6e759b012b8f510054d69cb51be7e17406aba47d
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2022-05-10 11:35:28 +0530

    Add files via upload

---
+++ src/test/java/io/github/selcukes/playwright/PlaywrightDemoTest2.java
@@ -0,0 +1,22 @@
+package io.github.selcukes.playwright;
+
+import com.microsoft.playwright.Page;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlaywrightDemoTest2 extends TestFixtures {
+    @Test
+    void shouldReturnInnerHTML() {
+        page.setContent("<div>hello</div>");
+        assertEquals("hello", page.innerHTML("css=div"));
+    }
+
+    @Test
+    void shouldClickButton() {
+        Page popup = page.waitForPopup(() -> {
+            page.evaluate("window.open('about:blank');");
+        });
+        assertEquals("about:blank", popup.url());
+    }
+}
---
+++ src/test/java/io/github/selcukes/playwright/TestFixtures.java
@@ -0,0 +1,37 @@
+package io.github.selcukes.playwright;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TestFixtures {
+    // Shared between all tests in the class.
+    Playwright playwright;
+    Browser browser;
+
+    @BeforeAll
+    void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+    }
+
+    @AfterAll
+    void closeBrowser() {
+        playwright.close();
+    }
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+}
