645e7022a36169d5beda8a20837fd94ce6d727f3
Ramesh Babu <rprudhvi@outlook.com>
2022-05-10 11:49:47 +0530

    Video and Screenshot

--- src/test/java/io/github/selcukes/playwright/TestFixtures.java
+++ src/test/java/io/github/selcukes/playwright/TestFixtures.java
@@ -3,6 +3,9 @@ package io.github.selcukes.playwright;
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
 
+import java.nio.file.Paths;
+import java.util.UUID;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestFixtures {
     // Shared between all tests in the class.
@@ -26,12 +29,16 @@ class TestFixtures {
 
     @BeforeEach
     void createContextAndPage() {
-        context = browser.newContext();
+        context =  browser.newContext(new Browser.NewContextOptions().setRecordVideoDir(Paths.get("videos/")));
+
         page = context.newPage();
     }
 
     @AfterEach
     void closeContext() {
+        page.screenshot(new Page.ScreenshotOptions()
+                .setPath(Paths.get("target/screenshot" + UUID.randomUUID() + ".png"))
+                .setFullPage(true));
         context.close();
     }
 }
